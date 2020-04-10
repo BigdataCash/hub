3bac3c3076da007ac971ca217473b01dd3e861b4
Martkist Team <team@martkist.org>
2020-04-10 15:16:27 +0200

    Remove legacy ticker

--- src/API/martkistTiker/martkistTiker.service.js
+++ src/API/martkistTiker/martkistTiker.service.js
@@ -10,9 +10,6 @@ const baseApiURL = process.env.REACT_APP_MARTK_MN_API;
 /**---------------------------------------------------------------------------- */
 
 const PROVIDERS = {
-  coinmarketcap: `${baseApiURL}/curl?url="https://api.coinmarketcap.com/v1/ticker/martkist/"`,
-  coingecko:
-    "https://api.coingecko.com/api/v3/coins/martkist?localization=false&tickers=false&market_data=true&community_data=false&developer_data=false",
   api:
     `${baseApiURL}/ticker`
 };
@@ -42,40 +39,4 @@ const apiFetcher = (url, params, actionType) => {
       .catch(err => {
         throw err;
       })
-};
-
-
-const coingeckoFetcher = (url, params, actionType) => {
-  return dispatch =>
-    axios
-      .get(url, params)
-      .then(response => {
-        return response.data;
-      })
-      .then(data => {
-        const {
-          current_price: { usd, btc },
-          price_change_percentage_24h,
-          price_change_percentage_1h,
-          price_change_percentage_7d
-        } = data.market_data;
-
-        console.log("ACZ data -->", data);
-        if (actionType != null) {
-          dispatch({
-            type: actionType,
-            data: {
-              usd,
-              btc,
-              price_change_percentage_24h,
-              price_change_percentage_1h,
-              price_change_percentage_7d
-            }
-          });
-        }
-        return data;
-      })
-      .catch(err => {
-        throw err;
-      });
 };
